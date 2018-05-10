f59bf72efe655dd12641a55cf2788c4b277e9515
giaquinta29 <giaquinta29@gmail.com>
2018-05-09 22:34:00 -0300

    Arreglos sobre la clase helper

--- UCU-WorldCup/Controller/Helper.swift
+++ UCU-WorldCup/Controller/Helper.swift
@@ -25,28 +25,49 @@ class Helper {
      init(){}
     
     func startConfig() -> [Match]{
-        //objetos de prueba
-        let player1 = Player(name:"Luis Suarez",number: "9", team: "Barcelona", isTechnical: false)
-        let player2 = Player(name:"Edision Caviani",number: "21", team: "Paris Saint Germain", isTechnical: false)
-        let playerDt = Player(name:"Oscar Washington Tabarez",number: "DT", team: "Uruguay", isTechnical: true)
-        let playerDoc = Player(name:"Alejandro Pan",number: "DR", team: "Uruguay", isTechnical: true)
-        listPlayersUruguay.append(player1)
-        listPlayersUruguay.append(player2)
-        listPlayersUruguay.append(playerDt)
-        listPlayersUruguay.append(playerDoc)
-        let player3 = Player(name:"Mirkov Putin",number: "7", team: "FC Zenit", isTechnical: false)
-        let player4 = Player(name:"Valdimir Putin",number: "8", team: "FC Zenit", isTechnical: false)
-        listPlayersRusia.append(player3)
-        listPlayersRusia.append(player4)
+        //Uruguay Players
+        let luisSuarez = Player(name:"Luis Suarez",number: "9", team: "Barcelona", isTechnical: false)
+        let cavani = Player(name:"Edision Caviani",number: "21", team: "Paris Saint Germain", isTechnical: false)
+        let tabarez = Player(name:"Oscar Washington Tabarez",number: "DT", team: "Uruguay", isTechnical: true)
+        let pan = Player(name:"Alejandro Pan",number: "DR", team: "Uruguay", isTechnical: true)
+        listPlayersUruguay.append(luisSuarez)
+        listPlayersUruguay.append(cavani)
+        listPlayersUruguay.append(tabarez)
+        listPlayersUruguay.append(pan)
         
-        let team1 = Team(name: "Uruguay", emblem: "uruguay", players: listPlayersUruguay, acronym: "URU", nextMatches: nextMatchesUruguay)
-        let team2 = Team(name: "Russian", emblem: "rusia", players: listPlayersRusia, acronym: "RUS", nextMatches: nextMatchesRusia)
+        //Rusia Players
+        let playerRusia1 = Player(name:"Mirkov Putin",number: "7", team: "FC Zenit", isTechnical: false)
+        let playerRusia2 = Player(name:"Valdimir Putin",number: "8", team: "FC Zenit", isTechnical: false)
+        listPlayersRusia.append(playerRusia1)
+        listPlayersRusia.append(playerRusia2)
         
+        //Egipto Players
+        let playerEgipto1 = Player(name:"Mohamed Salah",number: "9", team: "Liverpool", isTechnical: false)
+        let playerEgipto2 = Player(name:"El Neni",number: "21", team: "Arsenal", isTechnical: false)
+        listPlayersEgipto.append(playerEgipto1)
+        listPlayersEgipto.append(playerEgipto2)
         
-        let stadium = Stadium(name: "Samara Arena", photo: "Samara-Arena")
+        //Arabia Players
+        let playerArabiaSaudita1 = Player(name:"Fahad Al-Muwallad",number: "9", team: "Levante FC", isTechnical: false)
+        let playerArabiaSaudita2 = Player(name:"Osama Hawsawi",number: "21", team: "Al-Hilal FC", isTechnical: false)
+        listPlayersArabiaSaudita.append(playerArabiaSaudita1)
+        listPlayersArabiaSaudita.append(playerArabiaSaudita2)
         
+        //Teams Creation
+        let teamUruguay = Team(name: "Uruguay", emblem: "uruguay", players: listPlayersUruguay, acronym: "URU", nextMatches: nextMatchesUruguay)
+        let teamRusia = Team(name: "Russian", emblem: "rusia", players: listPlayersRusia, acronym: "RUS", nextMatches: nextMatchesRusia)
+        let teamEgipto = Team(name: "Egipto", emblem: "egipto", players: listPlayersEgipto, acronym: "EGP", nextMatches: nextMatchesEgipto)
+        let teamArabia = Team(name: "Arabia Saudita", emblem: "arabia saudita", players: listPlayersArabiaSaudita, acronym: "ASA", nextMatches: nextMatchesArabiaSaudita)
+        
+        //Stadiums
+        let stadiumSamara = Stadium(name: "Samara Arena", photo: "Samara-Arena")
+        let stadiumKazan = Stadium(name: "Kazan Arena", photo: "Kazan-Arena")
+        
+        //Date of matches
         let date = inicializeDateMatch(dd: 08, mm:04, yyyy: 2018, hour:18, min:30)
+         let date2 = inicializeDateMatch(dd: 08, mm:07, yyyy: 2018, hour:18, min:30) //Fecha que aun no paso
         
+        //Events of match URU vs RUS
         let event1 = Event(type: "⚽️", minute: 7, description: "Edision Caviani", local: true)
         listEventsMatch1.append(event1)
         let event2 = Event(type: "⚽️", minute: 19, description: "Luis Suarez", local: true)
@@ -56,29 +77,29 @@ class Helper {
         let event4 = Event(type: "🍓", minute: 89, description: "Valdimir Putin", local: false)
         listEventsMatch1.append(event4)
         
+        //Matches
+        let uruVEgi = Match(team1: teamUruguay, team2: teamEgipto, stadium: stadiumSamara, date: date2, group: "A", event: listEventsMatch2, result: (0,0))
         
-        let match1 = Match(team1: team1, team2: team2, stadium: stadium, date: date, group: "A", event: listEventsMatch1, result: (2,0))
-        matches.append(match1)
+        let uruVAra = Match(team1: teamUruguay, team2: teamArabia, stadium: stadiumSamara, date: date2, group: "A", event: listEventsMatch2, result: (0,0))
         
-        team1.nextMatches.append(match1)
-        team1.nextMatches.append(match1)
-        team2.nextMatches.append(match1)
+        let rusVEgi = Match(team1: teamRusia, team2: teamEgipto, stadium: stadiumSamara, date: date2, group: "A", event: listEventsMatch2, result: (0,0))
+        //Next Matches of Rusia
+        teamRusia.nextMatches.append(rusVEgi)
         
-        let playerEgipto1 = Player(name:"Mohamed Salah",number: "9", team: "Liverpool", isTechnical: false)
-        let playerEgipto2 = Player(name:"El Neni",number: "21", team: "Arsenal", isTechnical: false)
-        listPlayersEgipto.append(playerEgipto1)
-        listPlayersEgipto.append(playerEgipto2)
-        let playerArabiaSaudita1 = Player(name:"Fahad Al-Muwallad",number: "9", team: "Levante FC", isTechnical: false)
-        let playerArabiaSaudita2 = Player(name:"Osama Hawsawi",number: "21", team: "Al-Hilal FC", isTechnical: false)
-        listPlayersArabiaSaudita.append(playerArabiaSaudita1)
-        listPlayersArabiaSaudita.append(playerArabiaSaudita2)
-        let team3 = Team(name: "Egipto", emblem: "egipto", players: listPlayersEgipto, acronym: "EGP", nextMatches: nextMatchesEgipto)
-        let team4 = Team(name: "Arabia Saudita", emblem: "arabia saudita", players: listPlayersArabiaSaudita, acronym: "ASA", nextMatches: nextMatchesArabiaSaudita)
-        let stadium2 = Stadium(name: "Kazan Arena", photo: "Kazan-Arena")
-        let date2 = inicializeDateMatch(dd: 08, mm:07, yyyy: 2018, hour:18, min:30)
+        let uruVRus = Match(team1: teamUruguay, team2: teamRusia, stadium: stadiumSamara, date: date, group: "A", event: listEventsMatch1, result: (2,0))
+        matches.append(uruVRus)
+        //Next Matches of Uruguay
+        teamUruguay.nextMatches.append(uruVEgi)
+        teamUruguay.nextMatches.append(uruVAra)
         
-        let match2 = Match(team1: team3, team2: team4, stadium: stadium2, date: date2, group: "A", event: listEventsMatch2, result: (0,0))
-        matches.append(match2)
+        
+        
+        
+        let egiVAra = Match(team1: teamEgipto, team2: teamArabia, stadium: stadiumKazan, date: date2, group: "A", event: listEventsMatch2, result: (0,0))
+        matches.append(egiVAra)
+        
+       
+        
         return matches
     }
     
